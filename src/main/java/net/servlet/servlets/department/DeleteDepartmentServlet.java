c23cf9f893a7b2e9a48152e958e2dbfef447fc50
Xa3uK <hazardsales@gmail.com>
2022-02-10 20:36:21 +0200

    DepartmentManageService added

--- src/main/java/net/servlet/servlets/department/DeleteDepartmentServlet.java
+++ src/main/java/net/servlet/servlets/department/DeleteDepartmentServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.SneakyThrows;
 import net.servlet.connection.DataBaseConnection;
+import net.servlet.services.DepartmentManageService;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -26,11 +27,8 @@ public class DeleteDepartmentServlet extends HttpServlet {
     @SneakyThrows
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Long id = Long.valueOf(req.getParameter("id"));
-        Class.forName("org.postgresql.Driver");
-        Connection con = DataBaseConnection.getInstance().getConnection();
-        PreparedStatement st = con.prepareStatement("delete from department where id=?");
-        st.setLong(1, id);
-        int execute = st.executeUpdate();
+        DepartmentManageService dms = new DepartmentManageService();
+        int execute = dms.deleteDepartment(id);
         if (execute > 0) {
             req.setAttribute("isDelete", Boolean.TRUE);
         }
--- src/main/java/net/servlet/servlets/department/UpdateDepartmentServlet.java
+++ src/main/java/net/servlet/servlets/department/UpdateDepartmentServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.SneakyThrows;
 import net.servlet.connection.DataBaseConnection;
+import net.servlet.entities.Department;
+import net.servlet.services.DepartmentManageService;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -28,14 +30,8 @@ public class UpdateDepartmentServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Long id = Long.valueOf(req.getParameter("id"));
         String name = req.getParameter("name");
-
-        Class.forName("org.postgresql.Driver");
-        Connection con = DataBaseConnection.getInstance().getConnection();
-        String sql = "update department set name=? where id=?";
-        PreparedStatement st = con.prepareStatement(sql);
-        st.setString(1, name);
-        st.setLong(2, id);
-        int execute = st.executeUpdate();
+        DepartmentManageService dms = new DepartmentManageService();
+        int execute = dms.updateDepartment(name, id);
         if (execute > 0) {
             req.setAttribute("isUpdate", Boolean.TRUE);
         }
